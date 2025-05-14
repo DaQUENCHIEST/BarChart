93c99c8009ce2b27128e6619611668cfee903710
DaQUENCHIEST <119827276+DaQUENCHIEST@users.noreply.github.com>
2025-05-14 15:16:16 -0400

    Bro

--- BarChartRunner.java
+++ BarChartRunner.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class BarChartRunner {
@@ -7,7 +8,7 @@ public class BarChartRunner {
 
       //create the scanner
       try {
-        File inputFile = new File("GameOfThrones.txt");
+        File inputFile = new File("GameOfThrones.txt"); // input file
         Scanner sc = new Scanner(inputFile);
 
       // create the bar chart
@@ -15,7 +16,7 @@ public class BarChartRunner {
       String xAxis = sc.nextLine();
       String source = sc.nextLine();
       BarChart chart = new BarChart(title, xAxis, source);
-      chart.setCaption("2018");
+      chart.setCaption("Scene#"); // needs to be changed to dynamic, or I need to find both the start and the end. 
 
       // add the bars to the bar chart
          while(sc.hasNextLine()){
@@ -24,22 +25,31 @@ public class BarChartRunner {
             if(countLine.isEmpty()) continue;
             int numLines = Integer.parseInt(countLine);
 
-
+            ArrayList<Bar> bars = new ArrayList<>(); // initalizies the list of bar objects.
             for (int i = 0; i < numLines && sc.hasNextLine(); i++) {
                String characterLine = sc.nextLine();
                String[] parts = characterLine.split(",");
 
-               if (parts.length < 5) {
+               if (parts.length < 5) { // checks to make sure that the line the scanner is on has important information
                   System.out.println("Skipping malformed line: " + characterLine);
                   continue;
                }          
 
                String name = parts[1];
-               int value = Integer.parseInt(parts[3]);
+               String value = parts[3];
+               int val = Integer.parseInt(parts[3]);
                String group = parts[2];
 
-               chart.add(name, value, group);
+               bars.add(new Bar(name, val, group)); // adds the information taken from the txt file to the list
             }
+
+            bars.sort((a, b) -> b.getValue() - a.getValue());
+
+
+            for (int i = 0; i < Math.min(10, bars.size()); i++) {
+               Bar b = bars.get(i);
+               chart.add(b.getName(), b.getValue(), b.getCategory());
+            }  
          }
          // draw the bar chart
          StdDraw.setCanvasSize(1000, 700);
